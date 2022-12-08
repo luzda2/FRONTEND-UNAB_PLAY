c17aa968989aaf52e4b03f1b72cc1a0c87851f71
mariafdacevedo <113567795+mariafdacevedo@users.noreply.github.com>
2022-12-08 18:48:59 -0500

    Modificación archivo html

--- demo/js/app.js
+++ demo/js/app.js
@@ -1,26 +1,27 @@
 const app=Vue.createApp({
     data(){
         return{
+            id:"",
             categoria:"",
             evento:"",
             equipolocal:"",
             equipovisitante:"",
             marcador:"",
             partidos:[],
-            buscaCategoria:"",
-            buscaEquipo:"",
-            buscaEvento:"",
-            partido:{}
+            categoriaBuscar:"",
+            eventoBuscar:"",
+            equipoBuscar:""
         }
     },
     
     methods:{
         guardarPartido(){
-            const endpoint="http://localhost:8080/partido/guardar/";
+            const endpoint="http://localhost:8080/partido/guardar";
             const opciones={
                 method:"POST",
                 headers:{'Content-Type':'application/json'},
                 body:JSON.stringify({
+                    id:this.id,
                     categoria:this.categoria,
                     evento:this.evento,
                     equipolocal:this.equipolocal,
@@ -30,24 +31,37 @@ const app=Vue.createApp({
             };
 
             fetch(endpoint,opciones).then(async response=>{
-
                 Swal.fire({
                     position: 'top-end',
                     icon: 'success',
-                    title: 'Partido Guardado',
+                    title: 'Usuario  Guardado!',
                     showConfirmButton: false,
                     timer: 2500
-                  });
+                  })
+                  
+
+                this.id="";
                 this.categoria="";
                 this.evento="";
                 this.equipolocal="";
                 this.equipovisitante="";
-                this.marcador=""
+                this.marcador="";
+                             
             })
-            
+
         },
+        
+        verPartidos(){
+            const endpoint="http://localhost:8080/partido/consultar";
+            const opciones={method:"GET"}
+
+            fetch(endpoint,opciones).then(async response=>{
+                this.partidos=await response.json();
+            })
+        },
+
         buscarxcategoria(){
-            const endpoint="http://localhost:8080/partido/buscarpartidoxcategoria/"+this.buscaCategoria;
+            const endpoint="http://localhost:8080/partido/buscarpartidoxcategoria/"+this.categoriaBuscar;
             const opciones={method:"GET"};
 
             fetch(endpoint,opciones).then(async response=>{
@@ -56,7 +70,7 @@ const app=Vue.createApp({
 
         },
         buscarxevento(){
-            const endpoint="http://localhost:8080/partido/buscarpartidoxevento/"+this.buscaEvento;
+            const endpoint="http://localhost:8080/partido/buscarpartidoxevento/"+this.eventoBuscar;
             const opciones={method:"GET"};
 
             fetch(endpoint,opciones).then(async response=>{
@@ -65,14 +79,14 @@ const app=Vue.createApp({
 
         },
         buscarxequipo(){
-            const endpoint="http://localhost:8080/partido/buscarpartidoxequipo/"+this.buscaEquipo;
+            const endpoint="http://localhost:8080/partido/buscarpartidoxequipo/"+this.equipoBuscar;
             const opciones={method:"GET"};
 
             fetch(endpoint,opciones).then(async response=>{
                 this.partido=await response.json();
-            })
+            }) 
 
-        }        
+        }      
     }
 });
 app.mount("#aplicacion");
